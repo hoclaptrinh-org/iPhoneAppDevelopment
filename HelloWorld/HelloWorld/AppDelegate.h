b319d00119359c55e2554e4500b6635d54e6f7b8
Tui Coding <tuicoding@gmail.com>
2015-06-16 14:57:44 +0700

    Hello World

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Tui Coding on 6/16/15.
+//  Copyright (c) 2015 Tui Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Tui Coding on 6/16/15.
+//  Copyright (c) 2015 Tui Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
